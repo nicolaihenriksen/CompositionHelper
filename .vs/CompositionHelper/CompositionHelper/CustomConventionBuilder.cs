7770f279b8b8373926435141e89b5acecbd6fd45
Nicolai Henriksen <nicolai.h80@gmail.com>
2016-12-05 21:39:08 +0100

    Initial check in

---
+++ .vs/CompositionHelper/CompositionHelper/CustomConventionBuilder.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Composition.Convention;
+using System.Reflection;
+
+namespace Nicolai.Utils.Composition.CompositionHelper
+{
+    /// <summary>
+    /// Custom convention builder used to filter out certain exported types from an assembly.
+    /// 
+    /// This is used when adding an assembly to the composition, but wanting to compose some implementations in directly.
+    /// In standard MEF this could be done with ComposeExport(), but since that is not included in Microsoft.Composition
+    /// we have to filter out the types we want to override up front.
+    /// </summary>
+    internal class CustomConventionBuilder : ConventionBuilder
+    {
+
+        private readonly List<Type> ignoredTypes = new List<Type>();
+
+        public CustomConventionBuilder(params Type[] ignoredTypes)
+        {
+            if (ignoredTypes != null)
+            {
+                this.ignoredTypes.AddRange(ignoredTypes);
+            }
+        }
+
+        public override IEnumerable<Attribute> GetCustomAttributes(Type reflectedType, MemberInfo member)
+        {
+            if (this.ignoredTypes.Contains(reflectedType))
+            {
+                return new Attribute[0];
+            }
+            return base.GetCustomAttributes(reflectedType, member);
+        }
+
+    }
+}
---
+++ .vs/CompositionHelper/CompositionHelper/InstanceExportDescriptorProvider.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Composition.Hosting.Core;
+
+namespace Nicolai.Utils.Composition.CompositionHelper
+{
+    /// <summary>
+    /// Export descriptor used to allow us to add our own instances (i.e. stubs) into the composition.
+    /// </summary>
+    internal class InstanceExportDescriptorProvider : ExportDescriptorProvider
+    {
+        private readonly object instance;
+
+        public InstanceExportDescriptorProvider(object instance)
+        {
+            this.instance = instance;
+        }
+
+        public override IEnumerable<ExportDescriptorPromise> GetExportDescriptors(CompositionContract contract, DependencyAccessor descriptorAccessor)
+        {
+            var temp = new Type[0]; // Figure out whether this.instance is an instance of type contract.ContractType
+            if (temp.Where(t => Equals(t, contract.ContractType)).Any())
+            {
+                yield return new ExportDescriptorPromise(contract, contract.ContractType.FullName, true, NoDependencies, dependencies => ExportDescriptor.Create((context, operation) => this.instance, NoMetadata));
+            }
+        }
+
+    }
+}
